24df22068fe604a08d1b236ea82f17196bf4de5b
Enzo-Ruan <133675847+Enzo-Ruan@users.noreply.github.com>
2024-08-20 20:53:48 -0300

    Add files via upload

---
+++ Algoritmo C++/aula-01.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+main(){
+
+    char nome[20] = "Enzo";
+    int idade = 17;
+    float peso = 84.2;
+
+    printf("Bem vindo %s sua idade e %i e seu peso e %f.", nome ,idade,peso);
+}
